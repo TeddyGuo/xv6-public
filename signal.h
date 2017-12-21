0d32c1c717dc78fe9469f48423c335a010cc58f9
TeddyGuo <error35643527@gmail.com>
2017-12-21 16:26:16 +0800

    signal.h updated

--- signal.h
+++ signal.h
@@ -1,15 +1,7 @@
 #ifndef __XV6_SIGNAL
 #define __XV6_SIGNAL
 
-#define _sigfpe_firstest
-#define _sigfpe_timing
-#define _sigfpe_secondtest
-
-#define SIGHUP 0
-#define SIGINT 1
-#define SIGFPE 2
-#define SIGKILL 3
-#define SIGCLD 4
+#define SIGFPE 0
 
 typedef void (*sighadler_t)(int);
 
